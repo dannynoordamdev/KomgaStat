123f34a65264ca3c3db1f6ab80cb06ed299a4da9
dannynoordamdev <dannynoordam.dev@gmail.com>
2025-04-12 17:29:49 +0200

    removed unneccesary stuff

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,21 +1,10 @@
 import React, { useState, useEffect, useCallback } from "react";
-import { auth } from "../../Firebase/firebaseConfig";
-import { onAuthStateChanged } from "firebase/auth";
-import useKomgaData from "../Hooks/useKomgaData";
+
 import DisplayComicSeries from "../Components/DisplayComicSeries";
 import "../Styling/Dashboard.css";
 
 const Dashboard = () => {
-    const [user, setUser] = useState(null);
-
-    //Gebruik makende van de useKomgaData Hook
-    const { latestSeries, loading, error, retrievedKey, retrievedUrl } = useKomgaData(user);
-  
-
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, setUser);
-        return unsubscribe; // Bij het uitloggen van de gebruiker ruimen we resources op.
-    }, []);
+    
 
 
 
